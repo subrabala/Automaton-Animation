0ab82e89e9d00c6dc615df64f06883324a80b645
subrabala <subrabala_dash@srmap.edu.in>
2023-05-26 22:23:22 +0530

    completed-js-logic

--- script.js
+++ script.js
@@ -2,11 +2,8 @@ const gridContainer = document.getElementById("gridContainer");
 
 const rows = 30;
 const cols = 60;
-
 const Rule = 182;
 const binary = Rule.toString(2);
-console.log(binary);
-console.log(binary[0]);
 
 function getNeighbourValue(row, col) {
   const getCellColor = (r, c) =>
@@ -21,5 +18,25 @@ function getNeighbourValue(row, col) {
 
   return topLeft + top + topRight;
 }
+function generatePattern() {
+  for (let i = 1; i <= rows; i++) {
+    for (let j = 1; j <= cols; j++) {
+      const gridItem = document.createElement("div");
+      gridItem.classList.add("grid-item");
+      if (i === 1 && j === 31) {
+        gridItem.classList.add("black-cell");
+      }
+      if (i > 1) {
+        const neighbourValue = parseInt(getNeighbourValue(i, j), 2);
+        console.log(neighbourValue);
+        const power = binary.length - neighbourValue - 1;
+        if (binary[power] == 1) {
+          gridItem.classList.add("black-cell");
+        }
+      }
+      gridContainer.appendChild(gridItem);
+    }
+  }
+}
 
-
+generatePattern()
